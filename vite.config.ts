beed021799d1016fcf7cc51d6bea64cc23829bf9
Shamil <zagirovshamil@gmail.com>
2024-09-03 12:50:40 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/test/",
-  build: {
-    rollupOptions: {
-      input: "src/main.tsx",
-    },
-  },
+  base: "",
 });
