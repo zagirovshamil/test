7a1811e4c5d61d95cdda80e83d5fc43590ad5077
Shamil <zagirovshamil@gmail.com>
2024-09-03 12:46:33 +0300

    add build object to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/test/",
+  build: {
+    rollupOptions: {
+      input: "src/main.tsx",
+    },
+  },
 });
